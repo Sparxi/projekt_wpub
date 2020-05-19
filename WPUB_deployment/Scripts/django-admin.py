f069cf9375f8b2fe7831dd9fdca17b782005daa0
Jakub Macko <32487693+Sparxi@users.noreply.github.com>
2020-05-20 00:23:35 +0200

    deployment ready

---
+++ WPUB_deployment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jakub\desktop\jakub\skola\6.semester\webpub\wpub\projekt_wpub\wpub_deployment\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
